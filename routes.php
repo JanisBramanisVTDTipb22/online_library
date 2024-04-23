7677c74f12549c222e34b9be169692c3e3e44d9f
JanisBramanisVTDTipb22 <127381689+JanisBramanisVTDTipb22@users.noreply.github.com>
2024-04-23 22:48:40 +0300

    Delete routes.php

--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$routes = [
-    "/" => "controllers/index.php",
-    "/create" => "controllers/create.php",
-    "/login" => "controllers/books/login.php",
-    "/logout" => "controllers/books/logout.php",
-    "/register" => "controllers/books/register.php"
-];
